1da7a6dddbfce1d6ad8d78d23e4d0198c0fa2737
Gruselhaus <nico@gruselhaus.com>
2019-04-29 20:59:55 +0200

    Adding Header to query.sql

--- query.sql
+++ query.sql
@@ -1,3 +1,17 @@
+-------------------------------------------------------------------------------
+-- Author       Nico Finkernagel
+-- Created      04/27/19 (mm/dd/yy)
+-- Purpose      Fetching all Bussines Partners + Banking information from 
+--              SAP Bussines One Database
+-- Copyright ©  2019, Gruselhaus, All Rights Reserved
+-------------------------------------------------------------------------------
+-- Modification History
+--
+-- 01/01/0000  developer full name  
+--      A comprehensive description of the changes. The description may use as 
+--      many lines as needed.
+-------------------------------------------------------------------------------
+
 SELECT
   T0.CardCode,
   T0.CardName,
